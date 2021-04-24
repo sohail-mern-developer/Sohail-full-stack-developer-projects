6fa277dfa51585b0a7c707f02b03379a827cc3c5
Sohail <inboxtosohail@gmail.com>
2021-04-24 12:12:45 -0700

    Update java script
    Email Validation

--- Project 1/script.js
+++ Project 1/script.js
@@ -16,6 +16,15 @@ function showSuccess(input) {
     formControl.className = 'form-control success';
 }
 
+// Check email is valid
+
+function isValidEmail(email) {
+    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+}
+
+// add event listner
+
 form.addEventListener('submit', function(e){
     e.preventDefault();
 
@@ -27,6 +36,8 @@ form.addEventListener('submit', function(e){
 
     if(email.value === '') {
         showError(email, 'Email is required')
+    } else if(!isValidEmail(email.value)) {
+        showError(email,'Email is Invalid');
     } else {
         showSuccess(email);
     }
