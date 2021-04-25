e25fff02b10c28f5b304d447350134eb4046cb9c
Sohail <inboxtosohail@gmail.com>
2021-04-25 04:15:36 -0700

    Update script.js
    Check length by array()

--- Project 1/script.js
+++ Project 1/script.js
@@ -18,9 +18,13 @@ function showSuccess(input) {
 
 // Check email is valid
 
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())){
+        showSuccess(input);
+    } else {
+        showError(input, `${getFieldId(input)} is not a valid Email`)
+    }
 }
 
 // fUNCTION TO CHECK IF REQUIRED field have data
@@ -35,10 +39,22 @@ function checkRequired(inputArray) {
     });
 }
 
-// Function to get the idof input field
+// Function to get the id of input field
 
 function getFieldId(input) {
     return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
+
+// funtion to ccheck length of input field
+
+function checkLength(input, min, max) {
+    if(input.value.length < min) {
+        showError(input, `${getFieldId(input)} needs to be atleast ${min} characters`);
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldId(input)} needs to be less than ${max} characters`);
+    } else {
+        showSuccess(input);
+    }
 }
 
 // add event listner
@@ -47,5 +63,7 @@ form.addEventListener('submit', function(e){
     e.preventDefault();
 
     checkRequired([username,email,password,password2]);
-   
+    checkLength(username,3,10);
+    checkLength(password,6,30);
+    checkEmail(email);
 });
