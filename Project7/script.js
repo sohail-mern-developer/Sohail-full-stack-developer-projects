2f20813b20fd7656c1ade2f2088a7ebb5fc60d2d
Sohail <inboxtosohail@gmail.com>
2021-06-10 11:29:33 -0700

    Project 7 puzzle game (half script complete))
    script.js

--- Project7/script.js
+++ Project7/script.js
@@ -1,82 +1,77 @@
-// Get DOM Elements
 const word = document.getElementById('word');
 const incorrectLetters = document.getElementById('incorrect-letters');
-const popup = document.getElementById('popup-container');
-const playBtn = document.getElementById('play-btn');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
+const popup = document.getElementById('popup-container');
+const playBtn = document.getElementById('play-btn');
 
-// Get DO< Elements for Hangman
-const figureParts = document.querySelectorAll('.figure-part');
+const figureParts = document.querySelectorAll('figure-part');
 
-// This is the Pool of Selectedd words
-const words = ["behavior","nice","changing","keep","frozen","plate",
-"pitch","dress","apartment","research","copy","herd",
-"would","highest","natural","longer","toy","play",
-"far","because","music","sleep","give","obtain",
-"daily","safe","eat","warn","bound","clothes",
-"strip","wing","fourth","shade","desk","bring",
-"mice","examine","fuel","what","of","enemy"];
+const words = ["sohail","aqib"];
 
-//const words = ['cat', 'no'];
+let selectedWord = words[Math.floor(Math.random() * words.length)];
 
-// Select a random word from random array
-const selectedWord = words[Math.floor(Math.random() * words.length)];
-
-//Tracking arays for correct / incorrect letters
-const correctLettersArray = ['a','b','c','d','e','i','z','t'];
+const correctLettersArray = [];
 const incorrectLettersArray = [];
 
-// Function to display the seleected word  in the DOM
 function displayWord() {
-    //Display the selected word
+    // Display the selected word
     word.innerHTML = `
         ${selectedWord
             .split('')
             .map(letter => `
-                <span class = "letter">
-                ${correctLettersArray.includes(letter) ? letter : ''}
+                <span class="letter" >
+                    ${correctLettersArray.includes(letter) ? letter : '' }
                 </span>
-            `)
+                `
+            )
             .join('')
         }
     `;
-    const innerWord = word.innerText.replace(/\n/g, '');
-    
-    //console.log(word.innerText);
-    //console.log(innerWord);
 
+    // Replace new line character and form inner word
+    const innerWord = word.innerText.replace(/\n/g, '');
+        
+    // Compare inner word to selected word, if it's the same then game over and user won
     if(innerWord === selectedWord) {
-        finalMessage.innerText = 'Cogratulations! You Won...';
+        finalMessage.innerText = 'Congratulations! You won!'
         popup.style.display = 'flex';
-        //console.log(innerWord, selectedWord);
     }
+
+};
+
+function showNotification() {
+    notification.classList.add('show');
+
+    setTimeout(() => {
+        notification.classList.remove('show');
+    }, 2000);
 }
 
-// Event Lisners
 window.addEventListener('keydown', e => {
-    //Check if key press is letter
     if(e.keyCode >= 65 && e.keyCode <= 90) {
-     const letter = e.key;  
-     // check if letter is in the selected word
+        const letter = e.key;
         if(selectedWord.includes(letter)) {
-     //check if letter is already in CORRECTlETTERSaRRAY
-        if(!correctLettersArray.includes(letter)) {
-            correctLettersArray.push(letter)
-            displayWord();
-        }else {
-            showNotification();
-        }
+            if(!correctLettersArray.includes(letter)) {
+                correctLettersArray.push(letter);
+
+                displayWord();
+            } else {
+                showNotification();
+            }
         } else {
             if(!incorrectLettersArray.includes(letter)) {
                 incorrectLettersArray.push(letter);
-
                 updateIncorrectLetters();
             } else {
                 showNotification();
             }
         }
     }
+});
+
+playBtn.addEventListener('click', e => {
+    correctLettersArray.splice(0);
 });
 
 displayWord();
