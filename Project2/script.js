004ee86d133856550743abd7badb43b765e477c3
Sohail <inboxtosohail@gmail.com>
2021-06-21 10:06:15 -0700

    update

--- Project2/script.js
+++ Project2/script.js
@@ -1,60 +1,65 @@
-//GET DOM ELEMENTS
 const container = document.querySelector('.container');
 const seats = document.querySelectorAll('.row .seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+const movieName = document.getElementById('movie-name');
+const tPrice = document.getElementById('ticket-price');
+const selSeats = document.getElementById('sel-seats');
+const totalPrice = document.getElementById('total-price'); 
 
-// setPrice onLoad
 populateUI();
 
 let ticketPrice = +movieSelect.value;
 
-
 function populateUI() {
-    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-    if(selectedSeats !== null && selectedSeats.length > -1) {
+    const selectedSeats = JSON.parse(localStorage.getItem('seatIndex'));
+    if(selectedSeats !== null && selectedSeats.length > 0) {
         seats.forEach((seat, index) => {
             if(selectedSeats.indexOf(index) > -1) {
                 seat.classList.add('selected');
             }
         });
+        const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+        console.log(selectedMovieIndex);
+        if(selectedMovieIndex !== null) {
+            movieSelect.selectedIndex = selectedMovieIndex;
     }
-    const selectedMovieIndex = localStorage.getItem('setMovieIndex');
-    if(selectedMovieIndex !== null) {
-        movieSelect.selectedIndex = selectedMovieIndex;
     }
 }
 
-//update Seats Count
 function updateSelectedCount() {
-    const selectedSeats = document.querySelectorAll('.row .seat.selected');
-    const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
-    //console.log(seatIndex);
-    const selectedSeatsCount = selectedSeats.length;
-    count.innerText = selectedSeatsCount;
-    total.innerText = selectedSeatsCount * ticketPrice;
-    localStorage.setItem('selectedSeats', JSON.stringify(seatIndex));
+    if(movieSelect.value !== '') {
+        const selectedSeats = document.querySelectorAll('.row .seat.selected');
+        const seatIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+        console.log(seatIndex);
+        const selectedSeatsCount = selectedSeats.length;
+        count.innerText = selectedSeatsCount;
+        total.innerText = selectedSeatsCount * ticketPrice;
+        movieName.innerText = movieSelect.options[movieSelect.selectedIndex].text;
+        tPrice.innerText = ticketPrice;
+        selSeats.innerText = selectedSeatsCount;
+        totalPrice.innerText = selectedSeatsCount * ticketPrice;
+        localStorage.setItem('seatIndex', JSON.stringify(seatIndex));
+        console.log();
+    }
 }
 
-// set movie data
-
-function setMovieData(setMovieIndex, setMoviePrice) {
-    localStorage.setItem('setMovieIndex', setMovieIndex);
-    localStorage.setItem('setMoviePrice', setMoviePrice);
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('moviePrice', moviePrice);
 }
 
-// call event on click
 container.addEventListener('click', e => {
-    if(e.target.classList.contains('seat') && (
-        !e.target.classList.contains('occupied'))
-    ) {
-        e.target.classList.toggle('selected');
-        updateSelectedCount();
+    if(movieSelect.value !== '') {
+        if(e.target.classList.contains('seat') &&
+        !e.target.classList.contains('occupied')) {
+            e.target.classList.toggle('selected');
+            updateSelectedCount();
+        }
     }
 });
 
-// call event on change
 movieSelect.addEventListener('change', e => {
     ticketPrice = e.target.value;
     updateSelectedCount();
@@ -62,4 +67,3 @@ movieSelect.addEventListener('change', e => {
 });
 
 updateSelectedCount();
-
--- Project8/script.js
+++ Project8/script.js
@@ -1,6 +1,7 @@
-const search = document.getElementById('search');
 const submit = document.getElementById('submit');
-const generate = document.getElementById('generate');
+const search = document.getElementById('search');
+const searchBtn = document.getElementById('search-btn');
+const generateBtn = document.getElementById('generate-btn');
 const resultsHeading = document.getElementById('results-heading');
 const meals = document.getElementById('meals');
 const selectedMeal = document.getElementById('selected-meal');
@@ -9,86 +10,90 @@ function searchMeal(e) {
     e.preventDefault();
 
     const searchText = search.value;
-
+    
     if(searchText.trim()) {
         fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${searchText}`)
             .then(res => res.json())
             .then(data => {
-                resultsHeading.innerHTML = `<h2>Search results for ${searchText}</h2>`;
-
-                if(data.meals === null) {
-                    resultsHeading.innerHTML = `<h2>No results found for ${searchText}</h2>`;
-                } else {
-                    meals.innerHTML = data.meals.map( meal => `
-                        <div class="meal"><img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
-                        <div class="meal-info" data-mealID="${meal.idMeal}">
+                //console.log(data);
+                resultsHeading.innerHTML = `<h2>Search results for ${searchText}</h3>`;  
+              if(data.meals === null) {
+                resultsHeading.innerHTML = `<h2>No results found for ${searchText}</h3>`;
+              } else {
+                    meals.innerHTML = data.meals.map(meal => `
+                        <div class = "meal"><img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+                        <div class = "meal-info" data-mealID = "${meal.idMeal}">
                             <h3>${meal.strMeal}</h3>
-                    </div>
-                    </div>
+                        </div>
+                        </div>
                     `)
                     .join('')
-                }
+                    
+                    
+              }
             });
             search.value = '';
-    }
-};
-
-function getMeal(mealID) {
-        fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
-            .then(res => res.json())
-            .then(data => {
-                const meal = data.meals[0];
-                displayMealDetails(meal);
-            });
+    } 
 }
 
 function displayMealDetails(meal) {
-    const inngridients = [];
+    const ingredients = [];
 
     for(let i = 1; i <= 20; i++) {
         if( meal[`strIngredient${i}`] ) {
-            inngridients.push(`${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]} `);
+            ingredients.push(`${meal[`strIngredient${i}`]} : ${meal[`strMeasure${i}`]} `);
         } else {
             break;
         }
     }
+    console.log(meal);
     selectedMeal.innerHTML = `
         <div class="selected-meal-details">
             <h1>${meal.strMeal}</h1>
             <img src = "${meal.strMealThumb}" alt = "${meal.strMeal}" />
             <div class = "selected-meal-info">
-                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ''}
+                ${meal.strCategory ? `<p>${meal.strCategory}</p>` : '' }
                 ${meal.strArea ? `<p>${meal.strArea}</p>` : ''}
             </div>
-            <div>
+            <div class="selected-meal-instructions">
                 <p>${meal.strInstructions}</p>
                 <h3>Ingredients</h3>
                 <ul>
-                    ${inngridients.map(ingredient => `<li>${ingredient}</li>`).join('')};
+                    ${ingredients.map(ingredient => `<li>${ingredient}</li>`).join('')}
                 </ul>
             </div>
         </div>
+
+        
     `;
-
     meals.innerHTML = '';
-    resultsHeading.innerHTML = '';
+    
+}
 
+
+function getMeal(mealID) {
+    fetch(`HTTPS://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealID}`)
+        .then(res => res.json())
+        .then(data => {
+            const meal = data.meals[0];
+            displayMealDetails(meal);
+        });
 }
 
 submit.addEventListener('click', searchMeal);
 
 meals.addEventListener('click', e => {
     const mealInfo = e.path.find(item => {
+        
        if(item.classList) {
             return item.classList.contains('meal-info');
         } else {
             return false;
         }
+        
     });
-
     if(mealInfo) {
         const mealID = mealInfo.getAttribute('data-mealID');
-        
-        getMeal(mealID);
+        getMeal(mealID); 
     }
 });
